e72f6f8bb6383cfb479d10a1f2325c59d1650d87
jankavka <jankavka@seznam.cz>
2025-04-21 11:55:41 +0200

    Refactoring, new features
    - Fixed function for geting next page in list of events
    - Refactored some code in api.jsx

--- src/pages/public/CalendarComplete.jsx
+++ src/pages/public/CalendarComplete.jsx
@@ -8,11 +8,11 @@ const CalendarComplete = () => {
   const [events, setEvents] = useState([]);
   const [filter, setFilter] = useState({
     limit: 10,
-    pageToken: undefined,
+    nextPageToken: undefined,
     singleEvents: true,
   });
   //
-  const [tokens, setTokens] = useState([""])
+  const [tokens, setTokens] = useState([""]);
   const [page, setPage] = useState(0);
 
   useEffect(() => {
@@ -20,45 +20,52 @@ const CalendarComplete = () => {
       setEvents(data.items)
     );
 
-    apiGet("/api/calendar/all-events", filter).then((data) =>
+    apiGet("/api/calendar/all-events", filter).then((data) => {
+      console.log(data);
       setFilter((prev) => {
-        return { ...prev, pageToken: data.nextPageToken };
-      })
-    );
+        return { ...prev, nextPageToken: data.nextPageToken };
+      });
+    });
   }, []);
 
+  useEffect(() => {
+    console.log(page, tokens);
+  }, [page, tokens]);
+
+  //moves events list to next page
   const nextPage = () => {
     apiGet("/api/calendar/all-events", filter).then((data) => {
-      setEvents(data.items)
-      if(page === 0){
-        setTokens(["", data.nextPageToken])
-        setPage(prev => prev + 1)
-      }else {
-        setTokens(prev => {return [...prev, data.nextPageToken]})
-        setPage(prev => prev + 1)
+      setEvents(data.items);
+      if (page === 0) {
+        setTokens(["", data.nextPageToken]);
+        setPage((prev) => prev + 1);
+        setFilter((prev) => {
+          return { ...prev, nextPageToken: data.nextPageToken };
+        });
+      } else {
+        if (data.nextPageToken != undefined) {
+          setTokens((prev) => {
+            return [...prev, data.nextPageToken];
+          });
+          setPage((prev) => prev + 1);
+          setFilter((prev) => {
+            return { ...prev, nextPageToken: data.nextPageToken };
+          });
+        }
       }
-    }
-    );
-  }
-
-  //prev page fn => COMPLETE!!!
-  const prevPage = () => {
-    setFilter(prev => {return {...prev, pageToken: tokens[page-2]}});
-    apiGet("/api/calendar/all-events", filter).then((data) => {
-      setEvents(data.items)
     });
-    setPage(prev => prev -1);
+  };
 
-    apiGet("/api/calendar/all-events", filter).then((data) =>
-      setFilter((prev) => {
-        return { ...prev, pageToken: data.nextPageToken };
-      })
-    );
-
-  }
-
-    
-  
+  //this dosnt work yet!!!!
+  const prevPage = () => {
+    apiGet("/api/calendar/all-events", {
+      limit: 10,
+      nextPageToken: tokens[tokens.length - 2],
+    }).then((data) => {
+      setEvents(data.items);
+    });
+    setTokens((prev) => prev.pop());
+  };
 
   return (
     <div className="container-calendar">
@@ -78,7 +85,10 @@ const CalendarComplete = () => {
         <button className="col-5 my-button-previous" onClick={() => prevPage()}>
           předchozí strana
         </button>
-        <button className="offset-1 col-5 my-button-next" onClick={() => nextPage()}>
+        <button
+          className="offset-1 col-5 my-button-next"
+          onClick={() => nextPage()}
+        >
           další strana
         </button>
       </div>
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,13 +2,12 @@ export const API_URL = "http://localhost:8080";
 // export const API_URL = "http://10.0.0.3:8080"
 
 export const apiGet = async (url, params) => {
-  
   const requestParams = Object.fromEntries(
     Object.entries(params || {}).filter(([_, value]) => value != null)
   );
   const requestOptions = {
     method: "GET",
-  }; 
+  };
   const apiUrl = `${API_URL}${url}?${new URLSearchParams(requestParams)}`;
   const json = await fetch(apiUrl, requestOptions).then((response) =>
     response.json()
